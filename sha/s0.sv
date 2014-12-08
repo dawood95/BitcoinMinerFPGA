6e8fe8ad533d584a24f7a4e013f8de78a45983b2
Everett Berry <epberry@purdue.edu>
2014-12-07 21:46:31 -0500

    Inital work on software

--- sha/s0.sv
+++ sha/s0.sv
@@ -1,16 +1,16 @@
 // $Id: $
-// File name:   Sig0.sv
-// Created:     11/29/2014
+// File name:   S0.sv
+// Created:     11/6/2014
 // Author:      Sheik Dawood
 // Lab Section: 337-03
 // Version:     1.0  Initial Design Entry
-// Description: Sigma function 0
+// Description: S0
 
 
-module s0(
-	  input wire [31:0]  i,
-	  output wire [31:0] o
+module S0(
+	 input wire [31:0] i,
+	 output wire [31:0] o
 	 );
-   assign o = {i[6:0],i[31:7]}^{i[17:0],i[31:18]}^{3'b0,i[31:3]};
+   assign o = {i[1:0],i[31:2]}^{i[12:0],i[31:13]}^{i[21:0],i[31:22]};
 
 endmodule // m
