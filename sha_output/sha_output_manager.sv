e6db3a0aea38a3c72f66c4924dd07fbb26efcd7e
Mentor Graphics <mg84@ee215lnx13.ecn.purdue.edu>
2014-12-09 06:45:51 -0500

    Added reset ability to output manager.

--- sha_output/sha_output_manager.sv
+++ sha_output/sha_output_manager.sv
@@ -7,41 +7,47 @@
 // Description: SHA256 Output Manager (Common for all SHA256 cores)
 
 module sha_output_manager
-#(
-  parameter NUM_CORES = 2
-)
-(
-  input wire clk, enable,
-  input wire [33*NUM_CORES - 1:0] data_in,
-  output wire [NUM_CORES - 1:0] flag,
-  output reg [31:0] golden_nonce
-);
-  
-  genvar i;
-  integer j[31:0];
-  reg [33*NUM_CORES - 1:0] rf;
-
-  always_ff @ (posedge clk) begin
-    if (enable) rf <= data_in;
-    else rf <= rf;
-  end
-  
-  generate
-    for (i = 0; i < NUM_CORES; i++) begin
-      assign flag[i] = rf[33*(i + 1) - 1];
+  #(
+    parameter NUM_CORES = 2
+    )
+   (
+    input wire 			    clk, n_rst, enable,
+    input wire [33*NUM_CORES - 1:0] data_in,
+    output wire [NUM_CORES - 1:0]   flag,
+    output reg [31:0] 		    golden_nonce
+    );
+   
+   genvar i, j, m;
+   integer k[31:0];
+   reg [33*NUM_CORES - 1:0] rf;
+   
+   
+   generate
+   for (m = 0; m < NUM_CORES; m++) begin
+     always_ff @ (posedge clk, negedge n_rst) begin
+       if (n_rst == 0) rf[33*(m+1) - 1:33*m] <= 33'd17179869183;
+       else if (enable == 1'b1) rf[33*(m+1) - 1:33*m] <= data_in[33*(m+1) - 1:33*m];
+       else rf[33*(m+1) - 1:33*m] <= rf[33*(m+1) - 1:33*m];
+     end
+   end
+   endgenerate
+   
+   generate
+   for (i = 0; i < NUM_CORES; i++) begin
+     assign flag[i] = data_in[33*(i + 1) - 1];
+   end
+   endgenerate
+   
+   generate
+   for (j = 0; j < 32; j++) begin
+       always_comb begin
+          golden_nonce[j] = 1'b0;
+          for (k[j] = 0; k[j] < NUM_CORES; k[j]++) begin
+          if (flag[k[j]] == 1)
+            golden_nonce[j] = rf[33*k[j] + j];
+          end
+       end
     end
-  endgenerate
-  
-  generate
-    for (i = 0; i < 32; i++) begin
-      always_comb begin
-        golden_nonce[i] = 1'b0;
-        for (j[i] = 0; j[i] < NUM_CORES; j[i]++) begin
-          if (flag[j[i]] == 1)
-            golden_nonce[i] = rf[33*j[i] + i];
-        end
-      end
-    end
-  endgenerate
-
+   endgenerate
+   
 endmodule  
