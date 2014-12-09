05bf95d09a7c24debb208bdc9c45e00f2c60720d
Mentor Graphics <mg84@ee215lnx13.ecn.purdue.edu>
2014-12-09 06:42:32 -0500

    Added reset functionality to hash checker and changed logic of output flag.

--- sha/hash_checker.sv
+++ sha/hash_checker.sv
@@ -9,6 +9,7 @@
 
 module hash_checker (
 		     input wire 	clk,
+                     input wire         n_rst,
 		     input wire [5:0] 	count,
 		     input wire [31:0] 	nonce, data_in, //first 32 bits of hash
 		     output wire [32:0] flag_plus_nonce
@@ -17,11 +18,13 @@ module hash_checker (
    wire 				flag;
    reg [31:0] 				nonce_stored;
    
-   assign flag = (data_in == 32'b0) ? 1'b1 : 1'b0;
+   assign flag = ~(count == 5'b0 && n_rst && data_in == 32'b0) ? 1'b0 : 1'b1; //inverted to make default value 0
    assign flag_plus_nonce = {flag, nonce_stored};
   
-   always_ff @ (posedge clk) begin
-      if(count == 1'b1) 
+   always_ff @ (posedge clk, negedge n_rst) begin
+      if (n_rst == 0)
+        nonce_stored <= 32'hFFFFFFFF;
+      else if(count == 1'b1) 
 	nonce_stored <= nonce;
       else 
 	nonce_stored <= nonce_stored;
